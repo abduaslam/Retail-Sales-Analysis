20aad0587ba34d8579c63986039e7f45f406420b
abduaslam <abduaslam098@gmail.com>
2026-03-05 17:46:32 +0200

    answer business quetions using MYSQL

--- sql/sales_analysis_queries.sql
+++ sql/sales_analysis_queries.sql
@@ -13,5 +13,22 @@ LIMIT 10 ;
 SELECT Item , sum(Quantity) as Total_Sold
 FROM practice2.cleaned_retail_sales
 GROUP BY Item 
-ORDER BY Total_Sold 
+ORDER BY Total_Sold ;
+
+-- What is the average customer spending per transaction?
+SELECT avg(`Total Spent`) AS average_spending
+FROM practice2.cleaned_retail_sales;
+
+-- Which customers spend the most money?
+SELECT `Customer ID`,sum(`Total Spent`) AS Total_Spent
+FROM practice2.cleaned_retail_sales
+GROUP BY `Customer ID`
+ORDER BY Total_Spent;
+
+-- Are online or offline stores generating more revenue?
+SELECT Location,sum(`Total Spent`) as more_revenue 
+FROM practice2.cleaned_retail_sales
+GROUP BY Location
+O
+
  
